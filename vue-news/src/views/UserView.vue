ce77bd20bd8b6560b43879caafbe651fdde34609
[전시개발팀]이창근 <changgeun.lee@lotte.net>
2022-11-17 15:34:19 +0900

    refact: 리팩토링 2 - 사용자 프로필 컴포넌트 공통화
    사용자 컴포넌트 데이터 흐름 처리1

--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -1,22 +1,19 @@
 <template>
   <div>
-    <p>name : {{ userInfo.id }}</p>
-    <p>karma : {{ userInfo.karma }}</p>
-    <p>created : {{ userInfo.created }}</p>
+    <user-profile></user-profile>
   </div>
 </template>
 
 <script>
+import UserProfile from "@/components/UserProfile";
 export default {
   name: "UserView",
-  computed: {
-    userInfo() {
-      return this.$store.state.user;
-    }
-  },
   created() {
     const userName = this.$route.params.id;
     this.$store.dispatch('FETCH_USER', userName);
+  },
+  components: {
+    UserProfile
   }
 };
 </script>
