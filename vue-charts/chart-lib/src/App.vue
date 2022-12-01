9d2afeaa14605411d8795b5378f0e0f8479b4b62
[전시개발팀]이창근 <changgeun.lee@lotte.net>
2022-12-01 19:18:02 +0900

    feat: vue-charts

---
+++ vue-charts/chart-lib/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <h1>Chart.js</h1>
+    <bar-chart></bar-chart>
+    <line-chart></line-chart>
+  </div>
+</template>
+
+<script>
+import BarChart from "@/components/BarChart";
+import LineChart from "@/components/LineChart";
+
+export default {
+  name: 'App',
+  components: {
+    BarChart
+  , LineChart
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ vue-charts/chart-lib/src/components/LineChart.vue
@@ -0,0 +1,40 @@
+<template>
+  <div style="width: 100%;">
+    <canvas id="lineChart"></canvas>
+  </div>
+</template>
+
+<script>
+import {Chart} from "chart.js/auto";
+
+export default {
+  name: "LineChart",
+  mounted() {
+    (async function () {
+      const labels = ["Jan","Feb","Mar","Apr","May","Jun","Jul"];
+      const data = {
+        labels: labels,
+        datasets: [{
+          label: 'My First Dataset',
+          data: [65, 59, 80, 81, 56, 55, 40],
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1
+        }]
+      };
+
+      new Chart(
+          document.getElementById('lineChart'),
+          {
+            type: 'line',
+            data: data,
+          }
+      );
+    })();
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
