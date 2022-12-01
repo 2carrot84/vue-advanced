06073d151ba1db14067a4addc941c1ac0e5ab94b
[전시개발팀]이창근 <changgeun.lee@lotte.net>
2022-12-01 19:34:34 +0900

    feat: plugin 화

--- vue-charts/chart-lib/src/App.vue
+++ vue-charts/chart-lib/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <h1>Chart.js</h1>
-    <bar-chart></bar-chart>
+    <bar-chart v-bind:propsdata="chartDataset"></bar-chart>
     <line-chart></line-chart>
   </div>
 </template>
@@ -12,6 +12,19 @@ import LineChart from "@/components/LineChart";
 
 export default {
   name: 'App',
+  data() {
+    return {
+      chartDataset: [
+        {year: 2010, count: 10},
+        {year: 2011, count: 20},
+        {year: 2012, count: 15},
+        {year: 2013, count: 25},
+        {year: 2014, count: 22},
+        {year: 2015, count: 30},
+        {year: 2016, count: 28},
+      ]
+    }
+  },
   components: {
     BarChart
   , LineChart
--- vue-charts/chart-lib/src/components/LineChart.vue
+++ vue-charts/chart-lib/src/components/LineChart.vue
@@ -5,32 +5,28 @@
 </template>
 
 <script>
-import {Chart} from "chart.js/auto";
-
 export default {
   name: "LineChart",
   mounted() {
-    (async function () {
-      const labels = ["Jan","Feb","Mar","Apr","May","Jun","Jul"];
-      const data = {
-        labels: labels,
-        datasets: [{
-          label: 'My First Dataset',
-          data: [65, 59, 80, 81, 56, 55, 40],
-          fill: false,
-          borderColor: 'rgb(75, 192, 192)',
-          tension: 0.1
-        }]
-      };
+    const labels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"];
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }]
+    };
 
-      new Chart(
-          document.getElementById('lineChart'),
-          {
-            type: 'line',
-            data: data,
-          }
-      );
-    })();
+    new this.$_Chart(
+        document.getElementById('lineChart'),
+        {
+          type: 'line',
+          data: data,
+        }
+    );
   }
 }
 </script>
