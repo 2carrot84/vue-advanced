63b8a48770f53d9e69bac3b44ea0e2fb343ee0e0
[전시개발팀]이창근 <changgeun.lee@lotte.net>
2022-12-05 14:01:11 +0900

    feat: common desingh pattern

---
+++ design/common/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <app-header :title="appTitle"></app-header>
+    <app-content :items="items" @renew="renewItems"></app-content>
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader";
+import AppContent from "@/components/AppContent";
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppContent
+  },
+  data() {
+    return {
+      appTitle: 'Common Approach',
+      items: [10, 20, 30],
+    }
+  },
+  methods: {
+    renewItems() {
+      this.items = [40, 50, 60];
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ design/common/src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  props: {
+    title: String
+  },
+
+}
+</script>
+
+<style scoped>
+
+</style>
